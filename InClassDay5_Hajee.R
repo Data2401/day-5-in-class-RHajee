82311201d3b2cb5df7ae5e787d4821a51ce16a6e
Rehana Hajee <hajeerehana@gmail.com>
2025-02-04 12:52:40 -0600

    Completed part two

--- InClassDay5_Hajee.R
+++ InClassDay5_Hajee.R
@@ -8,6 +8,7 @@
 # save/commit/push as usual
 # turn the repository in on Blackboard when you're done. 
 # The code should be able to run on my machine with no errors. (I will check!)
+library()
 
 
 ### PART ONE - Review of Objects ####
@@ -48,29 +49,47 @@ too_big
 
 # Create a variable `name_length` that holds how many letters (including spaces)
 # are in your name, using the variable you made above. (use the `nchar()` function)
+name_length = nchar(my_name)
 
 # Print the number of letters in your name
+name_length
 
 # Create a variable `now_doing` that is your name followed by "is programming!" 
 # (use the `paste()` function)
+now_doing = paste (my_name, 'is programming!')
 
 # Make the `now_doing` variable upper case
+library(stringr)
+str_to_upper(now_doing)
 
 # Pick two of your favorite numbers (between 1 and 100) and assign them to 
 # variables `fav_1` and `fav_2`
+fav_1 = 7
+fav_2 = 6
 
 # Divide each number by the square root of 201 and save the new value in the
 # original variable
+fav_1 = fav_1/(sqrt(201))
+fav_2 = fav_2/(sqrt(201))
 
 # Create a variable `raw_sum` that is the sum of the two variables. Use the 
 # `sum()` function for practice.
-
+raw_sum = sum(fav_1, fav_2)
+  
 # Create a variable `round_sum` that is the `raw_sum` rounded to 2 decimal places.
 # Use the `round()` function.
+round_sum = round(raw_sum, 2)
+round_sum
 
 # Create two new variables `round_1` and `round_2` that are your `fav_1` and 
 # `fav_2` variables rounded to 2 decimal places
+round_1 = round(fav_1, 2)
+round_2 = round(fav_2, 2)
 
 # Create a variable `sum_round` that is the sum of the rounded values
+sum_round = sum(round_1, round_2)
 
 # Print out both variables and compare. Are they the same?
+round_sum
+sum_round
+print('There is a 0.1 difference between round_sum and sum_round with round_sum being the larger value.')
